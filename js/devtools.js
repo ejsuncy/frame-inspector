58e1c8d5e36e2334cb32cd919a41def6c3113328
Dan Bunker <dbunker@simplifile.com>
2016-08-31 13:03:34 -0600

    Add proof of concept for adding frame text to div in the devtools panel

--- js/devtools.js
+++ js/devtools.js
@@ -1,9 +1,28 @@
+function appendRow (extPanelWindow, text){
+    var contentNode = extPanelWindow.document.getElementById('frames');
+    var node = extPanelWindow.document.createElement("div");
+    node.appendChild(document.createTextNode(text));
+
+    var atBottom = contentNode.scrollHeight - contentNode.scrollTop === contentNode.clientHeight;
+
+    contentNode.appendChild(node);
+    if (atBottom)
+        node.scrollIntoView();
+
+    extPanelWindow.setTimeout(function(){
+        appendRow(extPanelWindow, text + 1);
+    }, 500);
+}
+
 chrome.devtools.panels.create("FrameInspector",
     "",
     "/html/devtools_panel.html",
     function(panel){
         panel.onShown.addListener(function (extPanelWindow) {
-            extPanelWindow.document.getElementById('content').innerText = "But *this* text was set from the devtools.js script!";
+            // var regex = localStorage.getItem('regex');
+            extPanelWindow.setTimeout(function(){
+                appendRow(extPanelWindow, 0);
+            }, 1000);
         });
     }
 );
